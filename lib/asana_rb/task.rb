ab875fe699dc35e9684aca5d3ba4eb69ebb19f86
Zaiste λ <oh@zaiste.net>
2012-04-23 13:49:24 +0200

    added methods for Task: create, list, mark as done

--- lib/asana_rb/task.rb
+++ lib/asana_rb/task.rb
@@ -1,17 +1,39 @@
 module AsanaRb
   class Task < Base
-    def initialize(text, workspace, assignee)
-      @options = { 
-        body: {
-          data: { 
-            workspace: workspace, 
-            name: text, 
-            assignee: assignee 
-          }
-        }.to_json
-      }
+    class << self
+      def create(text, workspace, assignee)
+        options = { 
+          body: {
+            data: { 
+              workspace: workspace, 
+              name: text, 
+              assignee: assignee 
+            }
+          }.to_json
+        }
 
-      self.class.post('/tasks', @options.merge!(@@auth))
+        post('/tasks', options.merge!(@@auth))
+      end
+
+      # { project: '<ID>' } or { workspace: '<ID>' }
+      def list(params)
+        options = {}
+
+        entity = params.shift
+        get("/#{entity.first.to_s}s/#{entity.last}/tasks", options.merge!(@@auth))
+      end
+
+      def done(task)
+        options = { 
+          body: {
+            data: {
+                completed: true
+            }
+          }.to_json
+        }
+
+        put("/tasks/#{task}", options.merge!(@@auth))
+      end
     end
   end
 end
